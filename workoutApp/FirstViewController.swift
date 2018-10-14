e13918023f8820e766f6731734612c5bfe93ab72
Feng Guo <fguo81@gmail.com>
2018-10-14 17:26:04 -0500

    Initial Commit

---
+++ workoutApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  workoutApp
+//
+//  Created by Feng Guo on 10/14/18.
+//  Copyright © 2018 Feng Guo. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
