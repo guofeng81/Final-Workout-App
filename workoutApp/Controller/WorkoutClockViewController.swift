28a2641868c6320b94cf03d83d1313e9de6bcecd
Feng Guo <fguo81@gmail.com>
2018-11-05 17:41:25 -0600

    Try to commit to the New Repo

---
+++ workoutApp/Controller/WorkoutClockViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WorkoutClockViewController.swift
+//  workoutApp
+//
+//  Created by Feng Guo on 10/14/18.
+//  Copyright © 2018 Feng Guo. All rights reserved.
+//
+
+import UIKit
+
+class WorkoutClockViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
